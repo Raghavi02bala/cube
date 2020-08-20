4277b2f042d9816c1a9cd6643adf7c7924b54b4f
Raghavi02bala <raghavibalasubramania@gmail.com>
2020-08-20 13:18:27 +0530

    text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Home.css';
+import bg_web from '../video/bg_web.mp4';
 
 function Home() {
     return (
         <div>
-           <video src=""></video>
+            <video className="video__bg"
+                autoPlay
+                loop
+                muted
+            >
+                <source src={bg_web} type="video/mp4" />
+            </video>
+            <div class="header-overlay" className="home__heading">
+                <h1>Welcome To Bluray</h1>
+                <p>Find your dream job here.We help you create your better future.</p>
+            </div>
         </div>
     );
 }
